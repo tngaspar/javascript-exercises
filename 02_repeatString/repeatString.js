8ae84e30221391f0413e0afb44cd55e61531fe9d
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-11 14:56:49 +0000

    solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, numberRepetitions) {
+    if (numberRepetitions < 0) {
+        return 'ERROR';
+    }
 
+    let repeatedString = '';
+    
+    for (let i =0; i < numberRepetitions; i ++){
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
+
+// console.log(repeatString('hey',4))
 
 // Do not edit below this line
 module.exports = repeatString;
