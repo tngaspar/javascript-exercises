19353624a92180a35d869f912dc8cbaf08857835
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-11 15:59:26 +0000

    solve 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    reversedString = ""
 
+    for (i = string.length -1 ; i >= 0; i--){
+        reversedString += string[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToRemove) {
+    outputArray = []
+    arrayIter: for (i = 0; i < array.length ; i++){
+        for(arg of argsToRemove){
+            if (array[i] === arg){
+                continue arrayIter;
+            }
+        }
+        outputArray.push(array[i])
+    }
+    return outputArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
