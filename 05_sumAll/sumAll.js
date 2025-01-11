951e2e87df1cc66ba2695c3a3bf19f088cb6a297
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-11 16:30:16 +0000

    solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum < 0 || lastNum < 0) 
+        return 'ERROR';
+    if (firstNum !== Math.floor(firstNum) || lastNum !== Math.floor(lastNum))
+        return 'ERROR';
+    if (typeof firstNum !== 'number' || typeof lastNum !== 'number') 
+        return 'ERROR'
 
+    lowestNum = Math.min(firstNum, lastNum);
+    highestNum = Math.max(firstNum, lastNum);
+
+    sum = 0;    
+    for (let i = lowestNum; i <= highestNum; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
