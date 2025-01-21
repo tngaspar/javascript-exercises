113f0abc51a2e7e845e33bf2d06f2dbb5eae05fc
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-21 23:51:41 +0000

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	// return a ** b;
+
+  let total = 1;
+  for(let i = 0; i < b; i++)
+    total *= a;
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  else return n * factorial(n-1);
 };
 
 // Do not edit below this line
