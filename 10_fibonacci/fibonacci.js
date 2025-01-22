cadb0769c8dae596e50a9b6894e6814002c7a775
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-22 00:46:43 +0000

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS";
 
+    previousVal = 0;
+    currentVal = 1;
+    for (let i = 1; i < n; i++){
+        aux = currentVal;
+        currentVal += previousVal;
+        previousVal = aux;
+    }
+
+    return currentVal;
 };
 
 // Do not edit below this line
