de9337a083555ba6d8db894ded744c6047b80119
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-22 00:30:34 +0000

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // remove punctuation and spaces
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+    
+    for (let i=0; i < string.length / 2; i++){
+        if(string[i] !== string[string.length -1 - i]) return false;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
