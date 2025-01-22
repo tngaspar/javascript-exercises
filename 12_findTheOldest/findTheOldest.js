abdb2a09c5a1b892abf6b49e9be9106e2051df4f
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2025-01-22 01:17:50 +0000

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    let currentOldestAge = 0;
+    let currentOldest;
 
-};
+    for (person of peopleArray){
+        if (person.yearOfDeath)
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        else
+            personAge = (new Date()).getFullYear() - person.yearOfBirth;
+
+        if (personAge > currentOldestAge){
+            currentOldestAge = personAge;
+            currentOldest = person;
+        }
+    }
+
+    return currentOldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
